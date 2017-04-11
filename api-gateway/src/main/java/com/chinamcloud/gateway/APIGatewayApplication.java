2fe492fca9de2d2b99a8e7a251c799591623433e
刘凯 <kai8406@gmail.com>
2017-04-11 21:22:34 +0800

    add api-gateway

---
+++ api-gateway/src/main/java/com/chinamcloud/gateway/APIGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.chinamcloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringCloudApplication
+public class APIGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(APIGatewayApplication.class, args);
+	}
+
+}
