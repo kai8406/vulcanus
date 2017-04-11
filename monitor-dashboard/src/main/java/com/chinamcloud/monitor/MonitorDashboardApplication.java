75b1e7d4da1ed57f545b4c08587dcd0e1317559d
刘凯 <kai8406@gmail.com>
2017-04-11 23:02:20 +0800

    add

--- monitor-dashboard/src/main/java/com/chinamcloud/monitor/MonitorDashboardApplication.java
+++ monitor-dashboard/src/main/java/com/chinamcloud/monitor/MonitorDashboardApplication.java
@@ -1,16 +1,16 @@
 package com.chinamcloud.monitor;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
 @EnableTurbineStream
 @EnableHystrixDashboard
-@SpringCloudApplication
+@SpringBootApplication
 public class MonitorDashboardApplication {
-
 	public static void main(String[] args) {
+
 		SpringApplication.run(MonitorDashboardApplication.class, args);
 	}
 
