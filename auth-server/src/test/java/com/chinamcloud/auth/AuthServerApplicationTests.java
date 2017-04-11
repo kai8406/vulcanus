4dd93acd8c2036e749dab95ef981128f4ebeb14b
刘凯 <kai8406@gmail.com>
2017-04-11 23:21:06 +0800

    add

---
+++ auth-server/src/test/java/com/chinamcloud/auth/AuthServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chinamcloud.auth;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = AuthServerApplication.class)
+public class AuthServerApplicationTests {
+
+	@Test
+	public void contextLoad() {
+	}
+
+}
