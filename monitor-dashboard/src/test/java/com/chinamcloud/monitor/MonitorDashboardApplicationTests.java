34df778eab97721c06e08cb38d88ed6e053905a5
刘凯 <kai8406@gmail.com>
2017-04-11 23:02:09 +0800

    add

---
+++ monitor-dashboard/src/test/java/com/chinamcloud/monitor/MonitorDashboardApplicationTests.java
@@ -0,0 +1,15 @@
+package com.chinamcloud.monitor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = MonitorDashboardApplication.class)
+public class MonitorDashboardApplicationTests {
+
+	@Test
+	public void contextLoad() {
+	}
+}
