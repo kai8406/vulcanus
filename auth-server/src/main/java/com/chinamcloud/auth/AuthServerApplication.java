b577c83234e8db942eee9b068fb83928c42620cf
刘凯 <kai8406@gmail.com>
2017-05-20 12:07:30 +0800

    add

--- auth-server/src/main/java/com/chinamcloud/auth/AuthServerApplication.java
+++ auth-server/src/main/java/com/chinamcloud/auth/AuthServerApplication.java
@@ -1,15 +1,23 @@
 package com.chinamcloud.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
@@ -17,13 +25,50 @@ import org.springframework.security.oauth2.provider.token.store.InMemoryTokenSto
 
 import com.chinamcloud.auth.service.security.AuthUserDetailsService;
 
-@SpringCloudApplication
-@EnableResourceServer
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@SpringBootApplication
 public class AuthServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthServerApplication.class, args);
+	}
+
+	@Configuration
+	@EnableWebSecurity
+	protected static class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+		@Override
+		@Bean
+		public AuthenticationManager authenticationManagerBean() throws Exception {
+			return super.authenticationManagerBean();
+		}
+
+		@Autowired
+		private AuthUserDetailsService authUserDetailsService;
+
+		@Autowired
+		protected void registerAuthentication(final AuthenticationManagerBuilder auth) throws Exception {
+			auth.userDetailsService(authUserDetailsService);
+		}
+	}
+
+	@Configuration
+	@EnableResourceServer
+	protected static class ResourceServer extends ResourceServerConfigurerAdapter {
+
+		@Override
+		public void configure(HttpSecurity http) throws Exception {
+
+			http.csrf().disable();
+
+			http.authorizeRequests().antMatchers("/oauth/token").anonymous();
+
+			// // Require all GET requests to have client "read" scope
+			// http.authorizeRequests().antMatchers(HttpMethod.GET, "/**").access("#oauth2.hasScope('read')");
+			//
+			// // Require all POST requests to have client "write" scope
+			// http.authorizeRequests().antMatchers(HttpMethod.POST, "/**").access("#oauth2.hasScope('write')");
+		}
+
 	}
 
 	/**
@@ -35,11 +80,14 @@ public class AuthServerApplication {
 	 */
 	@Configuration
 	@EnableAuthorizationServer
+	@Order(Ordered.LOWEST_PRECEDENCE - 100)
 	protected static class OAuth2AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
 
 		private TokenStore tokenStore = new InMemoryTokenStore();
 
+		// http://stackoverflow.com/questions/26208087/spring-boot-oauth-2-0-userdetails-user-not-found
 		@Autowired
+		@Qualifier("authenticationManagerBean")
 		private AuthenticationManager authenticationManager;
 
 		@Autowired
@@ -48,8 +96,6 @@ public class AuthServerApplication {
 		@Override
 		public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
 
-			// 在令牌端点上定义了安全约束
-			// security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
 			security.allowFormAuthenticationForClients();
 		}
 
@@ -65,7 +111,6 @@ public class AuthServerApplication {
 		@Override
 		public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 
-			// 定义了授权和令牌端点和令牌服务
 			endpoints.tokenStore(tokenStore).authenticationManager(authenticationManager)
 					.userDetailsService(authUserDetailsService);
 		}
